a16e77a6a4ee4730b37d29ffd1f4dccd1f8696e0
Jevy <jevymira@outlook.com>
2025-03-10 16:11:51 -0700

    feat(Model): scaffold Offer entity type

---
+++ Model/Offer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model;
+
+public partial class Offer
+{
+    public int Id { get; set; }
+
+    public Guid WootId { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Photo { get; set; }
+
+    public bool IsSoldOut { get; set; }
+
+    public string Condition { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+}
---
+++ Model/WootComputersSourceContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model;
+
+public partial class WootComputersSourceContext : DbContext
+{
+    public WootComputersSourceContext()
+    {
+    }
+
+    public WootComputersSourceContext(DbContextOptions<WootComputersSourceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Offer> Offers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=WootComputersSource;Integrated Security=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Offer>(entity =>
+        {
+            entity.ToTable("Offer");
+
+            entity.Property(e => e.Category).HasMaxLength(50);
+            entity.Property(e => e.Condition).HasMaxLength(50);
+            entity.Property(e => e.Photo)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Title).HasMaxLength(100);
+            entity.Property(e => e.Url).HasMaxLength(150);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
