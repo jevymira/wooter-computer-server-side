9922fd466b03e6db05a3046703f8227cbe808954
Jevy <jevymira@outlook.com>
2025-03-13 10:54:17 -0700

    fix: enable CORS

--- Server/Program.cs
+++ Server/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(option => option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
