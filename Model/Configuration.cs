32f5f4199ab9d31c0ae7c197c012d030f218955f
Jevy <jevymira@outlook.com>
2025-03-15 11:03:58 -0700

    feat(Model): run migration to add Price property

--- Model/Configuration.cs
+++ Model/Configuration.cs
@@ -15,6 +15,17 @@ public partial class Configuration
 
     public short StorageSize { get; set; }
 
+    /// <value>
+    /// The price denominated in USD ($).
+    /// </value>
+    /// <remarks>
+    /// Decimal seems conventional to use as the SQL Server data type because it is:
+    /// (1) decimal floating-point rather than binary floating-point and
+    /// (2) 128-bit, for twice the precision of the double type.
+    /// For SQL-CLR type mapping, see: https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/sql/linq/sql-clr-type-mapping#NumericMapping
+    /// </remarks>
+    public decimal Price { get; set; }
+
     public int OfferId { get; set; }
 
     public virtual Offer Offer { get; set; } = null!;
