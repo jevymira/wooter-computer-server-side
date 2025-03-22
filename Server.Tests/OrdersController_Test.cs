2ecb31c2f2985023104977780a1eb18394e4c256
Jevy <jevymira@outlook.com>
2025-03-22 11:24:21 -0700

    test: implement arrange for get offer

---
+++ Server.Tests/OrdersController_Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using Server;
+using Server.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Tests
+{
+    public class OrdersController_Test
+    {
+        /// <summary>
+        /// Test the GetOffer() method.
+        /// </summary>
+        public async Task GetOffer()
+        {
+            // Arrange  
+            var options = new DbContextOptionsBuilder<DbContext>()
+                .UseInMemoryDatabase(databaseName: "WootComputers")
+            .Options;
+            using var context = new DbContext(options);
+            context.Add(new Offer()
+            {
+                Id = 1,
+                Category = "Desktops",
+                Title = "Dell Optiplex 7070",
+                IsSoldOut = false,
+                Condition = "Refurbished",
+                Url = "https://computers.woot.com/offers/dell-optiplex-7070-micro-desktop-mini-pc-5"
+            });
+            context.SaveChanges();
+            var controller = new OffersController(context);
+        }
+    }
+}
