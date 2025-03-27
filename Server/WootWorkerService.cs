c9ae2172785fd8f63058586d471e75f1339acd57
Jevy <jevymira@outlook.com>
2025-03-27 10:52:09 -0700

    feat: use background service to populate db
    Inject IServiceScopeFactory to create scope in which dependencies on
    WootServicce and DbContext are resolved.

--- Server/WootWorkerService.cs
+++ Server/WootWorkerService.cs
@@ -1,23 +1,46 @@
-﻿namespace Server
-{
-    public class WootWorkerService : BackgroundService
-    {
-        private readonly ILogger<WootWorkerService> _logger;
+﻿using Model;
+using Server.Services;
 
-        public WootWorkerService(ILogger<WootWorkerService> logger)
-        {
-            _logger = logger;
-        }
+namespace Server
+{
+    public class WootWorkerService(
+        ILogger<WootWorkerService> logger,
+        IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        private readonly ILogger<WootWorkerService> _logger = logger;
+        private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<WootComputersSourceContext>();
+                var service = scope.ServiceProvider.GetRequiredService<WootService>();
+
+                var feed = await service.GetComputers();
+                var wootOffers = await service.GetAllPropertiesForFeedItems(feed);
+                var offers = await service.BuildOffers(wootOffers);
+
+                foreach (var offer in offers)
+                {
+                    var temp = context.Offers.FirstOrDefault(o => o.WootId == offer.WootId);
+
+                    if (temp is null)
+                    {
+                        context.Add(offer);
+                    }
+                }
+
+                await context.SaveChangesAsync();
+            }
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(1800000, stoppingToken);
             }
         }
     }
