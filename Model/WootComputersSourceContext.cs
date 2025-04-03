91527193e30aa9193a625be0cfd9f8125e782c85
Jevy <jevymira@outlook.com>
2025-04-02 17:25:03 -0700

    refactor: use config file for dbcontext connection

--- Model/WootComputersSourceContext.cs
+++ Model/WootComputersSourceContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Model;
 
@@ -22,8 +23,12 @@ public partial class WootComputersSourceContext : DbContext
     // FIXME: Add guard condition; required for EF Core migrations
     // but causes "multiple databases registered" error when unit testing.
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=WootComputers;Integrated Security=True;TrustServerCertificate=True");
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+        IConfigurationRoot configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
