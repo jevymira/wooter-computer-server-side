8616da14bc7a02f809ec96bedfc367fa7e02edd2
Jevy <jevymira@outlook.com>
2025-04-17 11:12:51 -0700

    style: use background service primary constructor

--- Server/WootWorkerService.cs
+++ Server/WootWorkerService.cs
@@ -1,21 +1,17 @@
-﻿using Model;
-using Server.Dtos;
-using Server.Services;
+﻿using Server.Services;
 
 namespace Server
 {
     public class WootWorkerService(
-        ILogger<WootWorkerService> logger,
-        IServiceScopeFactory scopeFactory) : BackgroundService
+        IServiceScopeFactory scopeFactory,
+        ILogger<WootWorkerService> logger
+    ) : BackgroundService
     {
-        private readonly ILogger<WootWorkerService> _logger = logger;
-        private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             using (var scope = scopeFactory.CreateScope())
             {
-                var context = scope.ServiceProvider.GetRequiredService<WootComputersSourceContext>();
                 var service = scope.ServiceProvider.GetRequiredService<WootService>();
 
                 await service
@@ -28,9 +24,9 @@ namespace Server
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                if (logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1800000, stoppingToken);
             }
