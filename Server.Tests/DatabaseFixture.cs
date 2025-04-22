37639c2177d398c5dc587d23812235506731c109
Jevy <jevymira@outlook.com>
2025-04-22 11:30:03 -0700

    test: use class fixture for offers dbcontext
    Move database context initialization and seeding logic into own class,
    to allow reuse across test cases.

---
+++ Server.Tests/DatabaseFixture.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Server.Tests;
+
+/// <remarks>
+/// Adapted from xUnit documentation at
+/// https://xunit.net/docs/shared-context#class-fixture.
+/// </remarks>
+public class DatabaseFixture : IDisposable
+{
+    public WootComputersSourceContext Context { get; set; }
+
+    public DatabaseFixture()
+    {
+        var options = new DbContextOptionsBuilder<WootComputersSourceContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var context = new WootComputersSourceContext(options);
+
+        context.Add(new Offer()
+        {
+            WootId = Guid.NewGuid(),
+            Category = "Desktops",
+            Title = "Dell Optiplex 7070",
+            Photo = "https://d3gqasl9vmjfd8.cloudfront.net/8697eca7-d3bc-451b-9111-9086936d8ecf.png",
+            IsSoldOut = false,
+            Condition = "Refurbished",
+            Url = "https://computers.woot.com/offers/dell-optiplex-7070-micro-desktop-mini-pc-5",
+            Configurations = new[] { new HardwareConfiguration()
+                    { MemoryCapacity = 16, StorageSize = 256 }
+                }
+        });
+
+        context.Add(new Offer()
+        {
+            WootId = Guid.NewGuid(),
+            Category = "Laptops",
+            Title = "Dell Latitude 7420",
+            Photo = "https://d3gqasl9vmjfd8.cloudfront.net/7819b57e-e82e-4656-a7e7-916f9606c0c7.jpg",
+            IsSoldOut = false,
+            Condition = "Refurbished",
+            Url = "https://computers.woot.com/offers/dell-latitude-7420-business-14-laptop-6",
+            Configurations = new[] { new HardwareConfiguration()
+                    { MemoryCapacity = 32, StorageSize = 256}
+                }
+        });
+
+        context.SaveChanges();
+
+        Context = context;
+    }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- Server.Tests/OrdersController_Test.cs
+++ Server.Tests/OrdersController_Test.cs
@@ -5,42 +5,27 @@ using Server.Dtos;
 
 namespace Server.Tests
 {
-    public class OrdersController_Test
+    public class OrdersController_Test : IClassFixture<DatabaseFixture>
     {
+        private readonly DatabaseFixture _fixture;
+
+        public OrdersController_Test(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
         /// <summary>
         /// Test the GetOffer() method.
         /// </summary>
         [Fact]
         public async Task GetOfferAsync()
         {
-            // Arrange  
-            var options = new DbContextOptionsBuilder<WootComputersSourceContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-            using var context = new WootComputersSourceContext(options);
-
-            context.Add(new Offer()
-            {
-                Id = 1,
-                Category = "Desktops",
-                Title = "Dell Optiplex 7070",
-                Photo = "https://d3gqasl9vmjfd8.cloudfront.net/8697eca7-d3bc-451b-9111-9086936d8ecf.png",
-                IsSoldOut = false,
-                Condition = "Refurbished",
-                Url = "https://computers.woot.com/offers/dell-optiplex-7070-micro-desktop-mini-pc-5",
-                Configurations = new[] { new HardwareConfiguration()
-                    { MemoryCapacity = 16, StorageSize = 256}
-                }
-            });
-            context.SaveChanges();
-
-            var controller = new OffersController(context);
-            OfferItemDto? offer_existing = null;
-            OfferItemDto? offer_notExisting = null;
+            // Arrange
+            var controller = new OffersController(_fixture.Context);
 
             // Act
-            offer_existing = (await controller.GetOffer(1)).Value;
-            offer_notExisting = (await controller.GetOffer(2)).Value;
+            OfferItemDto? offer_existing = (await controller.GetOffer(1)).Value;
+            OfferItemDto? offer_notExisting = (await controller.GetOffer(32767)).Value;
 
             // Assert
             Assert.NotNull(offer_existing);
@@ -86,10 +71,7 @@ namespace Server.Tests
                 }
             };
 
-            context.AddRange(expectedOffers);
-            context.SaveChanges();
-
-            var controller = new OffersController(context);
+            var controller = new OffersController(_fixture.Context);
 
             // Act
             var result = (await controller.GetOffers(null, [], [])).Value;
@@ -102,43 +84,7 @@ namespace Server.Tests
         public async Task GetOffersByCategoryAsync()
         {
             // Arrange
-            var options = new DbContextOptionsBuilder<WootComputersSourceContext>()
-                .EnableSensitiveDataLogging()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-            using var context = new WootComputersSourceContext(options);
-
-            context.Add(new Offer()
-            {
-                WootId = Guid.NewGuid(),
-                Category = "Desktops",
-                Title = "Dell Optiplex 7070",
-                Photo = "https://d3gqasl9vmjfd8.cloudfront.net/8697eca7-d3bc-451b-9111-9086936d8ecf.png",
-                IsSoldOut = false,
-                Condition = "Refurbished",
-                Url = "https://computers.woot.com/offers/dell-optiplex-7070-micro-desktop-mini-pc-5",
-                Configurations = new[] { new HardwareConfiguration() 
-                    { MemoryCapacity = 16, StorageSize = 256 } 
-                }
-            });
-
-            context.Add(new Offer()
-            {
-                WootId = Guid.NewGuid(),
-                Category = "Laptops",
-                Title = "Dell Latitude 7420",
-                Photo = "https://d3gqasl9vmjfd8.cloudfront.net/7819b57e-e82e-4656-a7e7-916f9606c0c7.jpg",
-                IsSoldOut = false,
-                Condition = "Refurbished",
-                Url = "https://computers.woot.com/offers/dell-latitude-7420-business-14-laptop-6",
-                Configurations = new[] { new HardwareConfiguration()
-                    { MemoryCapacity = 32, StorageSize = 256}
-                }
-            });
-
-            context.SaveChanges();
-
-            var controller = new OffersController(context);
+            var controller = new OffersController(_fixture.Context);
 
             // Act
             var result = (await controller.GetOffers("Desktops", [], [])).Value;
