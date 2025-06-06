032267ce086c4a56cd41f60716b0a3d7214591da
Jevy <jevymira@outlook.com>
2025-06-06 13:35:58 -0700

    docs(worker): provide woot sync function summary

--- Workers/SyncWithWoot.cs
+++ Workers/SyncWithWoot.cs
@@ -4,31 +4,37 @@ using Microsoft.Extensions.Logging;
 using Server.Services;
 using Server.Services.Extensions;
 
-namespace Workers
-{
-    public class SyncWithWoot
-    {
-        private readonly IServiceScopeFactory _scopeFactory;
-        private readonly ILogger _logger;
+namespace Workers;
 
-        public SyncWithWoot(IServiceScopeFactory scopeFactory, ILoggerFactory loggerFactory)
+public class SyncWithWoot
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger _logger;
+
+    public SyncWithWoot(IServiceScopeFactory scopeFactory, ILoggerFactory loggerFactory)
+    {
+        _scopeFactory = scopeFactory;
+        _logger = loggerFactory.CreateLogger<SyncWithWoot>();
+    }
+
+    /// <summary>
+    /// Run the Azure Function to synchronize the database with the feed of live Woot! offers,
+    /// at the start of each hour to account for Woot's daily request quota.
+    /// </summary>
+    [Function(nameof(SyncWithWoot))]
+    public async Task Run([TimerTrigger("0 0 */1 * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation("C# Timer trigger function executed at: {time}", DateTime.Now);
+        
+        if (myTimer.ScheduleStatus is not null)
         {
-            _scopeFactory = scopeFactory;
-            _logger = loggerFactory.CreateLogger<SyncWithWoot>();
+            _logger.LogInformation("Next timer schedule at: {time}", myTimer.ScheduleStatus.Next);
         }
 
-        [Function(nameof(SyncWithWoot))]
-        public async Task Run([TimerTrigger("0 0 */1 * * *")] TimerInfo myTimer)
+        using (var scope = _scopeFactory.CreateScope())
         {
-            _logger.LogInformation("C# Timer trigger function executed at: {time}", DateTime.Now);
-            
-            if (myTimer.ScheduleStatus is not null)
-            {
-                _logger.LogInformation("Next timer schedule at: {time}", myTimer.ScheduleStatus.Next);
-            }
-
-            using var scope = _scopeFactory.CreateScope();
             var service = scope.ServiceProvider.GetRequiredService<WootService>();
+
             try
             {
                 // Setup and Intermediate operations.
